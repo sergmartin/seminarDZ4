5dca5e252dc454590d874475e1ec2030db0a1822
Сергей <sergemartin@yandex.ru>
2022-09-30 10:06:32 +0300

    задача номер 25 создана

---
+++ task27/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 27
+Console.Write("Введите число положительное: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int r = 0;
+
+if (n != 0 && n >0 )
+{
+    string m = Convert.ToString(n);
+    for (int i = 0; i <= m.Length-1; i++)
+    {
+        //r =r + m[i];
+        r =r + int.Parse(m[i].ToString());
+    
+    }
+    Console.Write(r);
+    
+}
+else Console.Write("Ноль! или минус!!!");
